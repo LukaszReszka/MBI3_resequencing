667361295e85cc9f2b2310cfb9d10d26b2b0bb60
LukaszReszka <01143598@pw.edu.pl>
2023-12-16 01:16:40 +0100

    Reformatting and adding useful comments

--- count_variations_in_genes.py
+++ count_variations_in_genes.py
@@ -3,6 +3,7 @@ from vcf import Reader as vcf_reader
 from pyranges import PyRanges as pyrange
 from prettytable import PrettyTable
 
+# wczytanie pliku ze współrzędnymi genów ...
 genes = pyrange(
     read_csv(
         "refFlat.txt",
@@ -21,34 +22,42 @@ genes = pyrange(
             "exonEnds",
         ],
     )
-)["chr1"]
+)[
+    "chr1"
+]  # ... z jednoczesnym zawężeniem danych do analizowanej sekwencji
 
+# inicjalizacja słownika liczników wariatnów dla poszczególnych genów
 variations_in_genes_counter = dict()
 for gen_name in genes.name:
     variations_in_genes_counter[gen_name] = 0
 
+# sprawdzanie, w których genach występują kolejne warianty
 with open("coriell_chr1.vcf", "r") as f:
     variations = vcf_reader(f)
     for variation in variations:
         plus_strand_sel = (
             (genes.Strand == "+")
-            & (genes.Start <= (variation.POS-1))
-            & (genes.End >= (variation.POS-1))
+            & (
+                genes.Start <= (variation.POS - 1)
+            )  # odejmujemy 1, bo indeksowanie w pliku vcf
+            & (genes.End >= (variation.POS - 1))  # zaczyna się od 1, a w refFlat od 0
         )
         minus_strand_sel = (
             (genes.Strand == "-")
-            & (genes.Start >= (variation.POS-1))
-            & (genes.End <= (variation.POS-1))
+            & (genes.Start >= (variation.POS - 1))
+            & (genes.End <= (variation.POS - 1))
         )
         genes_containing_variation = genes[plus_strand_sel | minus_strand_sel]
-        if  len(genes_containing_variation) > 0:
+        if len(genes_containing_variation) > 0:  # zabezpieczenie przed "Empty PyRange"
             for gen in genes_containing_variation.name.unique():
                 variations_in_genes_counter[gen] += 1
 
+# utworzenie tabeli wyników
 results = PrettyTable(["Gene", "Number of Variations"])
 for key, val in variations_in_genes_counter.items():
     results.add_row([key, val])
 
+# wyświetlenie wyników i zapis do pliku results_tab.txt
 results_txt = results.get_string(sortby="Number of Variations", reversesort=True)
 with open("results_tab.txt", "w") as f:
     f.write(results_txt)
